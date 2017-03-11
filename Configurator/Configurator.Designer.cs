24d80175e2bc123ad18562373cd38d1766304e79
Kevin Wang <453587854@qq.com>
2017-03-11 11:00:48 +0800

    add configurator (run/stop, start on boot enable/disable)

---
+++ Configurator/Configurator.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Configurator
+{
+    partial class Configurator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_on_boot_button = new System.Windows.Forms.Button();
+            this.running_text = new System.Windows.Forms.Label();
+            this.start_on_boot_text = new System.Windows.Forms.Label();
+            this.running_button = new System.Windows.Forms.Button();
+            this.layout_refresher = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_on_boot_button
+            // 
+            this.start_on_boot_button.Location = new System.Drawing.Point(107, 51);
+            this.start_on_boot_button.Name = "start_on_boot_button";
+            this.start_on_boot_button.Size = new System.Drawing.Size(122, 31);
+            this.start_on_boot_button.TabIndex = 0;
+            this.start_on_boot_button.TabStop = false;
+            this.start_on_boot_button.UseVisualStyleBackColor = true;
+            this.start_on_boot_button.Click += new System.EventHandler(this.start_on_boot_button_Click);
+            // 
+            // running_text
+            // 
+            this.running_text.AutoSize = true;
+            this.running_text.Location = new System.Drawing.Point(9, 18);
+            this.running_text.Name = "running_text";
+            this.running_text.Size = new System.Drawing.Size(53, 13);
+            this.running_text.TabIndex = 1;
+            this.running_text.Text = "Running: ";
+            // 
+            // start_on_boot_text
+            // 
+            this.start_on_boot_text.AutoSize = true;
+            this.start_on_boot_text.Location = new System.Drawing.Point(9, 60);
+            this.start_on_boot_text.Name = "start_on_boot_text";
+            this.start_on_boot_text.Size = new System.Drawing.Size(74, 13);
+            this.start_on_boot_text.TabIndex = 2;
+            this.start_on_boot_text.Text = "Start on boot: ";
+            // 
+            // running_button
+            // 
+            this.running_button.Location = new System.Drawing.Point(107, 9);
+            this.running_button.Name = "running_button";
+            this.running_button.Size = new System.Drawing.Size(122, 31);
+            this.running_button.TabIndex = 3;
+            this.running_button.TabStop = false;
+            this.running_button.UseVisualStyleBackColor = true;
+            this.running_button.Click += new System.EventHandler(this.running_button_Click);
+            // 
+            // layout_refresher
+            // 
+            this.layout_refresher.Enabled = true;
+            this.layout_refresher.Interval = 3000;
+            this.layout_refresher.Tick += new System.EventHandler(this.layout_refresher_Tick);
+            // 
+            // Configurator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 93);
+            this.Controls.Add(this.running_button);
+            this.Controls.Add(this.start_on_boot_text);
+            this.Controls.Add(this.running_text);
+            this.Controls.Add(this.start_on_boot_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Configurator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configurator";
+            this.Load += new System.EventHandler(this.Configurator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_on_boot_button;
+        private System.Windows.Forms.Label running_text;
+        private System.Windows.Forms.Label start_on_boot_text;
+        private System.Windows.Forms.Button running_button;
+        private System.Windows.Forms.Timer layout_refresher;
+    }
+}
+
---
+++ Configurator/Configurator.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32.TaskScheduler;
+
+namespace Configurator
+{
+    public partial class Configurator : Form
+    {
+        public bool isRunning()
+        {
+            Process[] pname = Process.GetProcessesByName("ListaryWithWinKey");
+            return pname.Length > 0;
+        }
+
+        public bool isStartOnBoot()
+        {
+            using (TaskService ts = new TaskService())
+            {
+                Microsoft.Win32.TaskScheduler.Task task = ts.FindTask("ListaryWithWinKey", false);
+                return task != null;
+
+            }
+        }
+
+        public void setLayout()
+        {
+            if (isRunning())
+            {
+                this.running_text.Text = "Running: Yes";
+                this.running_button.Text = "Stop";
+            }
+            else
+            {
+                this.running_text.Text = "Running: No";
+                this.running_button.Text = "Run";
+            }
+            if (isStartOnBoot())
+            {
+                this.start_on_boot_text.Text = "Start on boot: Yes";
+                this.start_on_boot_button.Text = "Disable";
+            }
+            else
+            {
+                this.start_on_boot_text.Text = "Start on boot: No";
+                this.start_on_boot_button.Text = "Enable";
+            }
+        }
+
+        public Configurator()
+        {
+            InitializeComponent();
+        }
+
+        private void Configurator_Load(object sender, EventArgs e)
+        {
+            setLayout();
+        }
+
+        private void running_button_Click(object sender, EventArgs e)
+        {
+            if (isRunning())
+            {
+                Process[] processes = Process.GetProcessesByName("ListaryWithWinKey");
+                for (int i = 0; i < processes.Length; i++)
+                {
+                    try
+                    {
+                        processes[i].Kill();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Unable to terminate ListaryWithWinKey.exe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                if (!File.Exists("ListaryWithWinKey.exe"))
+                {
+                    MessageBox.Show("ListaryWithWinKey.exe not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                try
+                {
+                    Process.Start("ListaryWithWinKey.exe");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Process start failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            Thread.Sleep(1000);
+            setLayout();
+        }
+
+        private void layout_refresher_Tick(object sender, EventArgs e)
+        {
+            setLayout();
+        }
+
+        private void start_on_boot_button_Click(object sender, EventArgs e)
+        {
+            if (isStartOnBoot())
+            {
+                using (TaskService ts = new TaskService())
+                {
+                    Microsoft.Win32.TaskScheduler.Task task = ts.FindTask("ListaryWithWinKey");
+                    if (task != null)
+                    {
+                        ts.RootFolder.DeleteTask("ListaryWithWinKey");
+                    }
+                }
+            }
+            else
+            {
+                var exePath = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), "ListaryWithWinKey.exe");
+                if (!File.Exists(exePath))
+                {
+                    MessageBox.Show("ListaryWithWinKey.exe not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                using (TaskService ts = new TaskService())
+                {
+                    TaskDefinition td = ts.NewTask();
+                    td.RegistrationInfo.Description = "ListaryWithWinKey";
+                    td.Triggers.Add(new LogonTrigger());
+                    td.Settings.DisallowStartIfOnBatteries = false;
+                    td.Settings.StopIfGoingOnBatteries = false;
+                    td.Principal.RunLevel = TaskRunLevel.Highest;
+                    td.Actions.Add(new ExecAction(exePath, "", null));
+                    ts.RootFolder.RegisterTaskDefinition("ListaryWithWinKey", td);
+                }
+            }
+        }
+    }
+}
